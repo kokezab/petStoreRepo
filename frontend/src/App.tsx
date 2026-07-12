81a5481eb56327f699ad435d7a0ae0d24d91a6fb
kokezab <borislav.kokeza@planetsoft.eu>
2026-07-12 08:24:43 +0200

    App update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useFindPetsByStatus, useGetPetById } from '@/api/generated/pet/pet'
 
-export default function App() {
-  const { data, isLoading, error } = useGetPetById(9223372016900019091);
+function PetById({ id }: { id: number }) {
+  const { data, isLoading, error } = useGetPetById(id);
 
   if (isLoading) return <div>Loading pets...</div>
   if (error) return <div>Error loading pets</div>
@@ -9,6 +9,22 @@ export default function App() {
   return (
     <div>
       <h1>{data?.name}</h1>
+    </div>
+  )
+}
+
+export default function App() {
+  const { data, isLoading, error } = useFindPetsByStatus({ status: ['available'] });
+
+  if (isLoading) return <div>Loading pets...</div>
+  if (error) return <div>Error loading pets</div>
+
+  return (
+    <div>
+      <PetById id={1} />
+      {data?.map((pet) => (
+        <h2>{pet.name}</h2>
+      ))}
     </div>
   )
 }
