c96073f41577e366b71c47c577e0667f8cd52481
kokezab <borislav.kokeza@planetsoft.eu>
2026-07-20 11:28:47 +0200

    Obsolete localization deleted.

--- frontend/src/features/settings/SettingsPage.tsx
+++ frontend/src/features/settings/SettingsPage.tsx
@@ -1,30 +1,10 @@
-import { useState } from 'react';
-
-import { Select } from 'antd';
 import { ThemeToggle } from './components/ThemeToggle/ThemeToggle';
-import { useTranslation } from '@/loc/loc';
 
 export function SettingsPage() {
-  const [selectedLanguage, setSelectedLanguage] = useState('en');
-  const { t, locale, setLocale } = useTranslation();
-
   return (
     <div>
-      <button onClick={() => setLocale('en')}>English</button>
-      <button onClick={() => setLocale('sr')}>Serbian</button>
-      {t('cancel')}
-
-      <h2>{selectedLanguage === 'en' ? 'Settings' : 'Podešavanja'}</h2>
+      <h1>Settings</h1>
       <ThemeToggle />
-
-      <Select
-        value={selectedLanguage}
-        onChange={(value) => setSelectedLanguage(value)}
-        options={[
-          { value: 'en', label: 'English' },
-          { value: 'sr', label: 'Serbian' },
-        ]}
-      />
     </div>
   );
 }
